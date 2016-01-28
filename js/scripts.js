89cf7ed8aa98549f290db3190bd3686efc4b1b2c
Cassandra Culkins and Joey Arnstein <cassy.culkins@gmail.com>
2016-01-27 16:07:52 -0800

    stuff works kinda

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,49 @@
-function PuzzleSelector(answer, dashes) {
-  this.answer = answer;
-  this.dashes = dashes;
+var wordBank = ["cat", "dog", "parrot", "goldfish", "gecko", "turtle", "hamster", "rabbit", "horse", "frog", "ferret", "chinchilla", "hedgehog", "snake", "mouse"];
+
+function PuzzleSelector() {
+  this.answer = "";
+  this.dashes = [];
+  this.wordLength = 0;
+  this.wordbank = "";
 }
 
 PuzzleSelector.prototype.breakUpWord = function(){
- // var word = new puzzleSelector("boat", "----");
-var ourWord = this.answer;
-var ourSplitWord = ourWord.split("");
-return ourSplitWord;
+   // var word = new puzzleSelector("boat", "----");
+  var ourWord = this.answer;
+  var ourSplitWord = ourWord.split("");
+  return ourSplitWord;
 };
 
-PuzzleSelector.prototype.countLength = function(){
- // var word = new puzzleSelector("boat", "----");
-var ourWord = this.answer;
-var ourSplitWord = ourWord.split("");
-var wordLength = ourSplitWord.length;
-return wordLength;
+PuzzleSelector.prototype.createPuzzle = function(){
+   // var word = new puzzleSelector("boat", "----");
+  var ourWord = this.answer;
+  var ourSplitWord = ourWord.split("");
+  var wordLength = ourSplitWord.length;
+  this.wordLength = wordLength;
+  for (var index = 0; index < wordLength; index++) {
+    this.dashes.push("-");
+  }
+    return this.dashes;
 };
+PuzzleSelector.prototype.gameWord = function(){
+  var random = wordBank[Math.floor(wordBank.length * Math.random())];
+  this.answer = random;
+}
+
+
+
+
+quess method; (userGuess)
+  if this.answerArray.includes(guess);
+
+  puzzle.forEach(function(letter){
+    if(letter === userGuess)
+
+  })
+
+for(i=0; i >= this.wordLength; i++){
+  if(puzzleArray[i] === userGuess){
+    dashes[i] = userGuess
+  }
+  return dashes;
+}
