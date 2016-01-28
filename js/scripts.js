d126c21f939ee91dd589213c5353ca13e1161f81
Cassandra Culkins and Joey Arnstein <cassy.culkins@gmail.com>
2016-01-28 11:17:11 -0800

    add functioning userGuess method, and refractored else

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,68 @@
-var wordBank = ["cat", "dog", "parrot", "goldfish", "gecko", "turtle", "hamster", "rabbit", "horse", "frog", "ferret", "chinchilla", "hedgehog", "snake", "mouse"];
+
 
 function PuzzleSelector() {
   this.answer = "";
   this.dashes = [];
-  this.wordLength = 0;
-  this.wordbank = "";
-}
+  this.ourSplitWord = [];
+  this.guessedLettersRight = [];
+  this.guessedLettersWrong = [];
+};
+
+PuzzleSelector.prototype.gameWord = function(){
+  var wordBank = ["dragon"];
+  // var wordBank = ["cat", "dog", "parrot", "goldfish", "gecko", "turtle", "hamster", "rabbit", "horse", "frog", "ferret", "chinchilla", "hedgehog", "snake", "mouse"];
+  var random = wordBank[Math.floor(wordBank.length * Math.random())];
+  this.answer = random;
+};
 
 PuzzleSelector.prototype.breakUpWord = function(){
    // var word = new puzzleSelector("boat", "----");
   var ourWord = this.answer;
   var ourSplitWord = ourWord.split("");
-  return ourSplitWord;
+  this.ourSplitWord = ourSplitWord;
 };
 
 PuzzleSelector.prototype.createPuzzle = function(){
-   // var word = new puzzleSelector("boat", "----");
-  var ourWord = this.answer;
-  var ourSplitWord = ourWord.split("");
-  var wordLength = ourSplitWord.length;
-  this.wordLength = wordLength;
+  var wordLength = this.ourSplitWord.length;
   for (var index = 0; index < wordLength; index++) {
     this.dashes.push("-");
-  }
-    return this.dashes;
+  };
 };
-PuzzleSelector.prototype.gameWord = function(){
-  var random = wordBank[Math.floor(wordBank.length * Math.random())];
-  this.answer = random;
-}
-
-
-
-
-quess method; (userGuess)
-  if this.answerArray.includes(guess);
-
-  puzzle.forEach(function(letter){
-    if(letter === userGuess)
-
-  })
-
-for(i=0; i >= this.wordLength; i++){
-  if(puzzleArray[i] === userGuess){
-    dashes[i] = userGuess
+PuzzleSelector.prototype.userGuess = function(userGuess){
+  var wordLength = this.ourSplitWord.length;
+  if (this.ourSplitWord.includes(userGuess)){
+    for (var i = 0; i <= wordLength; i++){
+      if(this.ourSplitWord[i] === userGuess){
+        this.dashes[i] = userGuess;
+        console.log(this.dashes);
+        this.guessedLettersRight.push(userGuess);
+      }
+    }
+  } else {
+    this.guessedLettersWrong.push(userGuess);
   }
-  return dashes;
-}
+};
+
+
+// PuzzleSelector.prototype.guess = function(userGuess)
+//   if
+//
+// function User() {
+//
+// };
+
+// User.prototype.gameWord = function(){
+//   var answerArray= {"c", "a", "t"}
+// };
+//
+
+// quess method; (userGuess)
+//   if this.answerArray.includes(guess);
+//
+//   puzzle.forEach(function(letter){
+//     if(letter === userGuess)
+//
+//   })
+//
+//   return dashes;
+// }
--- specs/specs.js
+++ specs/specs.js
@@ -1,34 +1,46 @@
-// describe(Alphabet, function(){
-//   it("for a word it will return the position of a specified letter as the index for a inputted string"), function(){
-//     expect()
-//   }
 
 describe("PuzzleSelector", function() {
-  // it("assign the property of puzzle-answer to a word passed through the argument", function(){
-  //   var word = new PuzzleSelector();
-  //   expect(word.answer).to.equal("boat");
-  // });
-  it("will arrange the chosen word string into a puzzle array", function() {
-    var word = new PuzzleSelector();
-    word.answer = "cat";
-    expect(word.breakUpWord()).to.eql(["c", "a", "t"]);
-  });
-  it("will count the number of letters in the given answer and push '-''s to this.dashes for that number", function() {
-    var word = new PuzzleSelector();
-    word.answer = "cat";
-    expect(word.createPuzzle()).to.eql(["-", "-", "-"]);
-  });
   it("will choose a random word from the word bank", function() {
     var puzzle = new PuzzleSelector();
     puzzle.gameWord();
-    expect(wordBank.includes(puzzle.answer)).to.eql(true);
+    expect(puzzle.answer).to.equal("dragon");
+  });
+  it("will arrange the chosen word string into a puzzle array", function() {
+    var word = new PuzzleSelector();
+    word.gameWord();
+    word.breakUpWord();
+    expect(word.ourSplitWord).to.eql(["d","r","a","g","o","n"]);
+  });
+  it("will count the number of letters in the given answer and push '-''s to this.dashes for that number", function() {
+    var word = new PuzzleSelector();
+    word.gameWord();
+    word.breakUpWord();
+    word.createPuzzle();
+    expect(word.dashes).to.eql(["-", "-", "-", "-", "-", "-"]);
+  });
+  it("will use a loop to locate user letter to the ourSplitWord array", function() {
+    var word = new PuzzleSelector();
+    word.gameWord();
+    word.breakUpWord();
+    word.createPuzzle();
+    word.userGuess("a");
+    expect(word.dashes).to.eql(["-", "-", "a", "-", "-", "-"]);
+    expect(word.guessedLettersRight).to.eql(["a"]);
+  });
+  it("will use a loop to locate user letter to the ourSplitWord array", function() {
+    var word = new PuzzleSelector();
+    word.gameWord();
+    word.breakUpWord();
+    word.createPuzzle();
+    word.userGuess("b");
+    expect(word.dashes).to.eql(["-", "-", "-", "-", "-", "-"]);
+    expect(word.guessedLettersWrong).to.eql(["b"]);
   });
 });
-  // // it("will switch each piece of the array with a line")
-  //
-
-  // it("will compare a user letter to the 'letters' array")
-  //
-  // it("will uncover every 'puzzle' letter that a 'user' letter matches")
-
-  // it("will display piece of hangman for ever letter that a 'user' letter does not match to the puzzle")
+  // it("will keep track of every letter a user has guessed", function() {
+  //   var word = new PuzzleSelector();
+  //   word.gameWord();
+  //   word.breakUpWord();
+  //   word.createPuzzle();
+  //   word.userGuess("a");
+  // }
